c75bd239f0e8f01e58846f398885412789dad1fe
Kate Kremer <katrinakremer@gmail.com>
2019-07-11 19:03:48 -0400

    create restaurant component and make it a dumb component

--- no-you-pick/src/components/RestaurantList.js
+++ no-you-pick/src/components/RestaurantList.js
@@ -1,30 +1,42 @@
 import React, { Component } from "react";
+import _ from "lodash";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchRestaurants, getLocation } from "../actions";
-
+import Restaurant from './restaurant';
 
 class RestaurantList extends Component {
-  // componentDidMount() {
-  //   this.props.fetchRestaurants();
-  // }
+  componentDidMount() {
+    this.props.fetchRestaurants();
+  }
+
+  renderRestaurants(){
+    console.log(this.props);
+    return _.map(this.props.restaurants, restaurant =>{
+      return (
+           <Restaurant key={restaurant.id} restaurant={restaurant}/>
+        );
+    });
+  }
 
   render() {
     return (
       <div>
-        List of Restaurants
+        <h3> Restaurants</h3>
+        <ul className = 'restaurant-list-group'>
+        {this.renderRestaurants()}
+        </ul>
       </div>
     );
   }
 }
-//
-// function mapStateToProps(state) {
-//   return { restaurants: state.restaurants };
-// }
-//
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators({ fetchRestaurants }, dispatch);
-// }
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
-export default RestaurantList;
+
+function mapStateToProps(state) {
+  return { restaurants: state.restaurants };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchRestaurants }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
--- no-you-pick/src/components/restaurant.js
+++ no-you-pick/src/components/restaurant.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
+
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { fetchRestaurant, deleteRestaurant } from "../actions";
-
-class Restaurant extends Component{
+// import { fetchRestaurant, deleteRestaurant } from "../actions";
 
 
-  render (){
-    <div>Restaurant</div>
 
-    return (
-      <div>
-      <button
-        className="btn btn-danger pull-xs-right">Delete</button>
+const Restaurant =({restaurant}) => (
+      <div className='card text-center'>
+      <div className='card-body'>
+        <h5 className='card-title'>{restaurant.name}</h5>
+        <p className='card-text'>{restaurant.location.address}</p>
+        <button className="btn btn-danger pull-xs-right">Delete</button>
       </div>
-    );
-  }
-}
+      <div className='card-footer text-muted'>
+        {restaurant.categories[0].name}
+        </div>
+      </div>
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchRestaurant, deleteRestaurant}, dispatch)
-}
-export default connect(null, mapDispatchToProps) (Restaurant);
+    );
+
+export default Restaurant;
