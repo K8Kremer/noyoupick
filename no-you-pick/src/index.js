cebb6d96148fd28dce59b2660f93072387cee959
Kate Kremer <katrinakremer@gmail.com>
2019-07-11 11:39:13 -0400

    took out comments

--- no-you-pick/src/index.js
+++ no-you-pick/src/index.js
@@ -4,8 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//change back to App from './App'
 
-//change back to App
 ReactDOM.render(<App />, document.getElementById('root'));
 //test comment
