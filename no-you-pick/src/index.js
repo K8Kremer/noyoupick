b1166a7a2608ea2b32e6f519835c83cf0875b0a8
Kate Kremer <katrinakremer@gmail.com>
2019-07-11 11:14:10 -0400

    add title component and its css styling

--- no-you-pick/src/index.js
+++ no-you-pick/src/index.js
@@ -4,7 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//change back to App from './App'
 
-
+//change back to App
 ReactDOM.render(<App />, document.getElementById('root'));
 //test comment
