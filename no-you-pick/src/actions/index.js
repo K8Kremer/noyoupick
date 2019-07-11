8f7878da64a983946120ae5af60c826e0be63aa9
Ronnie <rscott910@outlook.com>
2019-07-11 18:19:21 -0400

    finished setVoters action

--- no-you-pick/src/actions/index.js
+++ no-you-pick/src/actions/index.js
@@ -3,20 +3,23 @@ import axios from "axios";
 export const FETCH_RESTAURANTS = "fetch_restaurants";
 export const FETCH_RESTAURANT = "fetch_restaurant";
 export const GET_LOCATION = "get_location";
-export const DELETE_RESTAURANT = "delete-restaurant";
+export const DELETE_RESTAURANT = "delete_restaurant";
+export const SET_VOTERS = "set_voters";
 
 const CLIENT_ID = "&client_id=AUGZLXPVOKAA0ZONPNCYDWLQBUL1FQLOF5NZWCBJXLQSUTET";
 const CLIENT_SECRET = "&client_secret=USTU1O2CLXSF11IG0UTDD2DJ1K10YHVXG3QXJVVUVQFJ5CTX";
 const VERSION = '&v=20190711';
 const CATEGORY_ID = '&categoryId=4d4b7105d754a06374d81259';
 const ROOT_URL = 'https://api.foursquare.com/v2/venues/search?';
+// HARD CODED PARLOUR VENUE_ID FOR NOW
+const VENUE_ID = '515cc981e4b0d533bbfbab35';
 
 const COMBINED_URL = `${ROOT_URL}${CLIENT_ID}${CLIENT_SECRET}${VERSION}${CATEGORY_ID}`
 // export function getLocation() {
-//
+//  const coords = ...TODO
 //   return {
 //     type: GET_LOCATION,
-//     payload:
+//     payload: coords
 //   }
 // }
 
@@ -30,10 +33,29 @@ export function fetchRestaurants(lat=35.99, lon=-78.89) {
   };
 }
 
+// HARD CODED PARLOUR VENUE_ID FOR NOW
 export function fetchRestaurant(){
-  const request = axios.get(`https://api.foursquare.com/v2/venues/VENUE_ID`)
+  const request = axios.get(`https://api.foursquare.com/v2/venues/${CLIENT_ID}${CLIENT_SECRET}${VERSION}${VENUE_ID}`)
+  console.log(request);
   return {
     type: FETCH_RESTAURANT,
     payload: request
+  };
+}
+
+export function deleteRestaurant() {
+
+  return {
+    type: DELETE_RESTAURANT,
+    payload: id
+  }
+}
+
+// get the number of voters from the user input via the form and set
+// to limit api response.venues
+export function setVoters(numberOfVoters) {
+  return {
+    type: SET_VOTERS,
+    payload: numberOfVoters
   }
 }
--- no-you-pick/src/reducers/reducer-Restaurants.js
+++ no-you-pick/src/reducers/reducer-Restaurants.js
@@ -1,13 +1,21 @@
 import _ from "lodash";
-import { FETCH_RESTAURANTS, FETCH_RESTAURANT, DELETE_RESTAURANT } from "../actions";
+import { FETCH_RESTAURANTS, FETCH_RESTAURANT, DELETE_RESTAURANT, SET_VOTERS } from "../actions";
 
-const defaultObject = {};
+const defaultObject = {
+  numVoters: 1
+};
 
 export default function (state = defaultObject, action){
   switch (action.type) {
     case FETCH_RESTAURANTS:
     console.log(action.payload)
       return _.mapKeys(action.payload.data.response.venues, "id");
+    case SET_VOTERS:
+      return {...state, numVoters: action.payload}
+    // case FETCH_RESTAURANT:
+    //   return {...state, {action.payload.data.response.venues.id}:}
+    // case DELETE_RESTAURANT:
+    //   return
     default:
       return state;
   }
