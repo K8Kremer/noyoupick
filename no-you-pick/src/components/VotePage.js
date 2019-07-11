a97df9be76a39b9439f7e471e75b2e6d3787b047
dposse <dposse.125@gmail.com>
2019-07-11 18:55:59 -0400

    Form is working

--- no-you-pick/src/components/VotePage.js
+++ no-you-pick/src/components/VotePage.js
@@ -1,51 +1,27 @@
 import React, { Component } from 'react';
 import Title from './title';
 import RestaurantList from './RestaurantList';
+import NumberOfPeopleForm from './NumberOfPeopleForm';
 import { connect } from 'react-redux';
 import LocationButton from './LocationButton';
 import { fetchRestaurants } from '../actions/index';
 import { bindActionCreators } from 'redux';
 
 class VotePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.clickHandler = this.clickHandler.bind(this);
-  }
-
-
-
-  renderField() {
-
-    //placeholder - here define how ReduxForm Field is implemented
-    return (
-      <div></div>
-    );
-  }
-
-  clickHandler(e) {
-    e.preventDefault();
-    this.props.fetchRestaurants()
-  }
-
   render() {
     return (
       <div>
         <Title />
         <LocationButton />
-        <form>
-          <input placeholder='Input number of people'></input>
-          <button onClick={this.clickHandler} className='btn btn-primary'>Find Restaurants</button>
-        </form>
+        <NumberOfPeopleForm />
         <RestaurantList />
-        {this.props.testProp}
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return { ... state };
+  return {...state};
 }
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchRestaurants }, dispatch);
--- no-you-pick/src/reducers/reducer-Restaurants.js
+++ no-you-pick/src/reducers/reducer-Restaurants.js
@@ -1,17 +1,16 @@
 import _ from "lodash";
 import { FETCH_RESTAURANTS, FETCH_RESTAURANT, DELETE_RESTAURANT, SET_VOTERS } from "../actions";
 
-const defaultObject = {
-  numVoters: 1
-};
+const defaultObject = {};
 
 export default function (state = defaultObject, action){
   switch (action.type) {
     case FETCH_RESTAURANTS:
     console.log(action.payload)
       return _.mapKeys(action.payload.data.response.venues, "id");
-    case SET_VOTERS:
-      return {...state, numVoters: action.payload}
+    // case SET_VOTERS:
+    //   console.log(action.payload)
+    //   return {...state, numVoters: action.payload}
     // case FETCH_RESTAURANT:
     //   return {...state, {action.payload.data.response.venues.id}:}
     // case DELETE_RESTAURANT:
