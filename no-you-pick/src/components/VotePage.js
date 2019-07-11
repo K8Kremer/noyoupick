7463f3ab11eba5748e3cfa47a87be92535ff3875
dposse <dposse.125@gmail.com>
2019-07-11 11:09:58 -0400

    Added dummy form in VotePage component, imported bootstrap to index.js

--- no-you-pick/src/components/VotePage.js
+++ no-you-pick/src/components/VotePage.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const VotePage = () => (
-  <div>votepage</div>
-);
+class VotePage extends Component {
+  renderField() {
+    
+    //placeholder - here define how ReduxForm Field is implemented
+    return (
+      <div></div>  
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div>VotePage</div>
+        <form>
+          <input placeholder='Input number of people'></input>
+          <button className='btn btn-primary'>Find Restaurants</button>  
+        </form> 
+      </div>
+    );
+  }
+}
 
 export default VotePage;
