ae3af6d47da566facbcab8912278c67f481f5fe8
Ronnie <rscott910@outlook.com>
2019-07-11 20:06:18 -0400

    added radius of 8 miles to the fetchRestaurants api request

--- no-you-pick/src/reducers/reducer-Restaurants.js
+++ no-you-pick/src/reducers/reducer-Restaurants.js
@@ -8,9 +8,9 @@ export default function (state = defaultObject, action){
     case FETCH_RESTAURANTS:
     console.log(action.payload)
       return _.mapKeys(action.payload.data.response.venues, "id");
-    // case SET_VOTERS:
-    //   console.log(action.payload)
-    //   return {...state, numVoters: action.payload}
+    case SET_VOTERS:
+      console.log(action.payload)
+      return {...state, numVoters: action.payload}
     // case FETCH_RESTAURANT:
     //   return {...state, {action.payload.data.response.venues.id}:}
     // case DELETE_RESTAURANT:
