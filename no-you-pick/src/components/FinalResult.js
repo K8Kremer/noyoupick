ea98016e7aeb9b941589847110ebc199a7447891
Kate Kremer <katrinakremer@gmail.com>
2019-07-12 11:07:09 -0400

    add bootstrap styling and css for Final Result page

--- no-you-pick/src/components/FinalResult.js
+++ no-you-pick/src/components/FinalResult.js
@@ -1,3 +1,4 @@
+import '../FinalResult.css';
 import React from 'react';
 import Title from './title';
 import { connect } from 'react-redux';
@@ -14,16 +15,26 @@ const FinalResult = ({ restaurant }) => {
       <Redirect from='/finalResult' to='/votePage' />
     );
   }
-  
+
   return (
     <div>
       {console.log(lastRestaurant)}
       <Title />
       {/* TODO: display final restaurant - different than current restaurant snippets */}
       {/* general layout - needs styling */}
-      <img src='https://images.freshop.com/1898839853949256474/eb93e944c27796a842f48d899224623d_medium.png' alt='' />
-      <h3>{lastRestaurant.name}</h3>
-      <p>{lastRestaurant.location.formattedAddress[0]}, {lastRestaurant.location.formattedAddress[1]}</p>
+      <div className='row'>
+        <div className='col'></div>
+        <div className='col-md-4'>
+          <div className='card text-center w-80 shadow p-3 mb-5 bg-white rounded'>
+            <img className='card-img-top'src='https://images.freshop.com/1898839853949256474/eb93e944c27796a842f48d899224623d_medium.png' alt='' />
+            <div className='card-body'>
+              <h3 className='card-text'>{lastRestaurant.name}</h3>
+              <p className='card-text'>{lastRestaurant.location.formattedAddress[0]}, {lastRestaurant.location.formattedAddress[1]}</p>
+            </div>
+          </div>
+      </div>
+      <div className='col'></div>
+    </div>
     </div>
   );
 };
