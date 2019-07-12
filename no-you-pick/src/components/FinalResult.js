9ff070fdf3ae51eef021e1ba30b4154cdfb88eab
dposse <dposse.125@gmail.com>
2019-07-12 10:29:43 -0400

    Final result template working, reroutes to votePage if we refresh app on finalResult page

--- no-you-pick/src/components/FinalResult.js
+++ no-you-pick/src/components/FinalResult.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import Title from './title';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const FinalResult = () => (
-  <div>
-    <Title />
-    {/* TODO: display final restaurant - different than current restaurant snippets */}
-  </div>
-);
+const FinalResult = ({ restaurant }) => {
+  //should only have one restaurant in restaurant object
+  // even if we have more than one, will only use first object's key
+  const restaurantId = Object.keys(restaurant)[0];
+  const lastRestaurant = restaurant[restaurantId];
 
-export default FinalResult;
+  if (lastRestaurant === undefined) {
+    return (
+      <Redirect from='/finalResult' to='/votePage' />
+    );
+  }
+  
+  return (
+    <div>
+      {console.log(lastRestaurant)}
+      <Title />
+      {/* TODO: display final restaurant - different than current restaurant snippets */}
+      {/* general layout - needs styling */}
+      <img src='https://images.freshop.com/1898839853949256474/eb93e944c27796a842f48d899224623d_medium.png' alt='' />
+      <h3>{lastRestaurant.name}</h3>
+      <p>{lastRestaurant.location.formattedAddress[0]}, {lastRestaurant.location.formattedAddress[1]}</p>
+    </div>
+  );
+};
+
+
+// should only get to FinalResult component when restaurants only has one object left
+function mapStateToProps(state) {
+  return { restaurant: state.restaurants };
+}
+
+export default connect(mapStateToProps, null)(FinalResult);
